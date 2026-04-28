eef24452a7061494d73ebc4d005939fe444362b2
paulscc <paul.sc9@hotmail.com>
2026-04-28 08:03:52 -0500

    v5

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "rds_endpoint" {
   description = "Endpoint de la instancia RDS"
-  value       = aws_db_instance.rds_mysql.endpoint
+  value       = aws_db_instance.rds_postgresql.endpoint
 }
 
 output "rds_username" {
@@ -15,6 +15,6 @@ output "rds_db_name" {
 
 output "connection_string" {
   description = "String de conexión completa"
-  value       = "mysql://${var.db_username}:${var.db_password}@${aws_db_instance.rds_mysql.endpoint}/${var.db_name}"
+  value       = "postgresql://${var.db_username}:${var.db_password}@${aws_db_instance.rds_postgresql.endpoint}/${var.db_name}"
   sensitive   = true
 }
